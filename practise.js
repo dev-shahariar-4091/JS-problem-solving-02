8fa8d37aa3fc644305008515c0298cc1fe3f29ae
Shahariarislam <shahariarislam6309@gmail.com>
2025-09-27 19:21:14 +0600

    Cheak your age which is adult/moiner

--- practise.js
+++ practise.js
@@ -33,20 +33,34 @@
 // num(22);
 
 
-// 3. Find the Largest of Three Numbers 
+// 3. Find the Largest of Three Numbers  (complete)
 
-function largestNum(num1, num2, num3){
-    if(num1 > num2 && num1 > num3){
-        console.log("Num1 is the largest number : ", num1);
-    }
-    else if(num2 > num1 && num2 > num3){
-        console.log("Num2 is the largest number : ", num2);
-    }
-    else{
-        console.log("Num3 is the largest number : ", num3);
-    }
+// function largestNum(num1, num2, num3){
+//     if(num1 > num2 && num1 > num3){
+//         console.log("Num1 is the largest number : ", num1);
+//     }
+//     else if(num2 > num1 && num2 > num3){
+//         console.log("Num2 is the largest number : ", num2);
+//     }
+//     else{
+//         console.log("Num3 is the largest number : ", num3);
+//     }
+// }
+
+// largestNum(25, 30, 18);
+// largestNum(40, 510, 555);
+
+
+// 4. Check if a Person is an Adult 
+function adult (age){
+    if(age > 18 ){
+      console.log("You can voted : " , age);
+  }
+  else{
+      console.log("You can't voted : ", age)
+  }
 }
 
-largestNum(25, 30, 18);
-largestNum(40, 510, 555);
-
+adult(18);
+adult(16);
+adult(25);
