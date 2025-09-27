5b5f4ae2a96a49cdb1f2a3a08ac6ebfe412b4f58
Shahariarislam <shahariarislam6309@gmail.com>
2025-09-27 19:17:21 +0600

    Add largest three number problem is solve

--- practise.js
+++ practise.js
@@ -17,18 +17,36 @@
 //     num(1);
 //     num(-2);
 
-// 2. Check if a Number is Even or Odd
-function num(number){
-     if(number % 2 == 0){
-       console.log("This number is Even : ",number );
+// 2. Check if a Number is Even or Odd (complete)
+// function num(number){
+//      if(number % 2 == 0){
+//        console.log("This number is Even : ",number );
     
-     }
-     else{
-        console.log("This number is Odd" , number);
-    }
+//      }
+//      else{
+//         console.log("This number is Odd" , number);
+//     }
 
+// }
+
+// num(21);
+// num(22);
+
+
+// 3. Find the Largest of Three Numbers 
+
+function largestNum(num1, num2, num3){
+    if(num1 > num2 && num1 > num3){
+        console.log("Num1 is the largest number : ", num1);
+    }
+    else if(num2 > num1 && num2 > num3){
+        console.log("Num2 is the largest number : ", num2);
+    }
+    else{
+        console.log("Num3 is the largest number : ", num3);
+    }
 }
 
-num(21);
-num(22);
+largestNum(25, 30, 18);
+largestNum(40, 510, 555);
 
