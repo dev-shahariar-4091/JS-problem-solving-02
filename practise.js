15993192c4efb1ab8ef49aaa29373feaa2f7523a
Shahariarislam <shahariarislam6309@gmail.com>
2025-09-27 18:52:05 +0600

    solve the positive, negative and zero

---
+++ practise.js
@@ -0,0 +1,18 @@
+// 1. Check if a Number is Positive, Negative, or Zero
+
+function num(number){
+    
+    if(number > 0){
+        console.log("This number is positive : ", number);
+    }
+    else if(number < 0 ){
+        console.log("This numbe is negitive :", number);
+    }
+    else{
+        console.log("This number is zero : ", number);
+    }
+    }
+
+    num (0);
+    num(1);
+    num(-2);
