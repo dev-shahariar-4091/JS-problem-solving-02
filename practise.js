3be131c39a29d1747f0d36babc3d718ff8d098c7
Shahariarislam <shahariarislam6309@gmail.com>
2025-09-27 19:08:29 +0600

    Add the even or odd number probleming solve

--- practise.js
+++ practise.js
@@ -1,18 +1,34 @@
-// 1. Check if a Number is Positive, Negative, or Zero
+// 1. Check if a Number is Positive, Negative, or Zero (Complete)
 
-function num(number){
+// function num(number){
     
-    if(number > 0){
-        console.log("This number is positive : ", number);
-    }
-    else if(number < 0 ){
-        console.log("This numbe is negitive :", number);
-    }
-    else{
-        console.log("This number is zero : ", number);
-    }
+//     if(number > 0){
+//         console.log("This number is positive : ", number);
+//     }
+//     else if(number < 0 ){
+//         console.log("This numbe is negitive :", number);
+//     }
+//     else{
+//         console.log("This number is zero : ", number);
+//     }
+//     }
+
+//     num (0);
+//     num(1);
+//     num(-2);
+
+// 2. Check if a Number is Even or Odd
+function num(number){
+     if(number % 2 == 0){
+       console.log("This number is Even : ",number );
+    
+     }
+     else{
+        console.log("This number is Odd" , number);
     }
 
-    num (0);
-    num(1);
-    num(-2);
+}
+
+num(21);
+num(22);
+
